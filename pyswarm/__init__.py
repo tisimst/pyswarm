c7f8dd1724a6ad122c2d1dd9875949b15b8da01b
Sebastian Castillo-Hair <castillohair@gmail.com>
2015-08-04 13:16:49 -0500

    Increased version number to 0.7.

--- pyswarm/__init__.py
+++ pyswarm/__init__.py
@@ -3,14 +3,15 @@
 pyswarm: Particle swarm optimization (PSO) with constraint support
 ================================================================================
 
-Author: Abraham Lee
-Copyright: 2013-2014
+Authors: Abraham Lee
+         Sebastian Castillo-Hair
+Copyright: 2013-2015
 
 """
 # from __future__ import absolute_import
 
 __author__ = 'Abraham Lee'
-__version__ = '0.6'
+__version__ = '0.7'
 
 from pyswarm.pso import *
     
